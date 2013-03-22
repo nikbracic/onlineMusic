b1f42de19ff428cf99094be1f04cc1959137ffa6
nikbracic <nikbracic@yahoo.com>
2013-03-22 18:17:41 +0100

    devise gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OnlineMusic::Application.config.secret_token = '8a3452f16c24f98984c21280b7acf3278c44da1f5eed09646c38f752fdd32eed57142ea7425d242a43d01aca662b81cbb0e3866563f216e25e688c25567a3a17'
